969237809287722116876666c9437ec7ea732754
何彥南 (yen-nan ho) <aaron1aaron2@gmail.com>
2025-07-28 21:57:52 +0800

    update env scripts

--- scripts/set_env/Win/set-env_conda.bat
+++ scripts/set_env/Win/set-env_conda.bat
@@ -1,11 +1,26 @@
 @Echo off
 
 rem Define here the path to your conda installation
-set CONDAPATH=C:\\Users\\User\\miniconda3\\
+set CONDAPATH=C:\Users\aaron\miniconda3
+
+rem Check if conda directory exists
+if not exist "%CONDAPATH%\" (
+    echo Error: Conda installation not found at %CONDAPATH%
+    echo Please modify the CONDAPATH in this script to point to your conda installation directory.
+    echo Common locations include:
+    echo   - C:\Users\%%USERNAME%%\miniconda3
+    echo   - C:\Users\%%USERNAME%%\Anaconda3
+    echo   - C:\ProgramData\Miniconda3
+    echo   - C:\ProgramData\Anaconda3
+    echo.
+    echo To find your conda path, you can run: where conda
+    pause
+    exit /b 1
+)
 
 rem Activate the conda environment
 rem Using call is required here, see: https://stackoverflow.com/questions/24678144/conda-environments-and-bat-files
-call %CONDAPATH%\Scripts\activate.bat
+call "%CONDAPATH%\Scripts\activate.bat"
 
 rem 使用 python 3.10 創環境，如我沒有 python 請先自行安裝
 call conda create --name benchmark python=3.10 -y
